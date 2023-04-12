4b1cbd1e8ee2d33933043c6e98b77d79053d1dba
Josias Buday Developer <josiasbudaydeveloper@gmail.com>
2023-04-12 00:10:23 -0300

    Updating accounts-service
    Changing to a database persistence

---
+++ back-end/accounts-service/src/models/account-model.ts
@@ -0,0 +1,46 @@
+import database from "../db";
+import { Optional, DataTypes, Model } from 'sequelize';
+import { IAccount } from "../interfaces/account-interfaces";
+import { AccountStatus } from "../interfaces/account-status";
+
+class Account extends Model<IAccount, Optional<IAccount, "id">> {
+  declare id: number;
+  declare name: string;
+  declare email: string;
+  declare password: string;
+  declare status: AccountStatus
+  declare domain: string;
+}
+
+export default Account.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING(60),
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.SMALLINT,
+    defaultValue: 100,
+    allowNull: false
+  },
+  domain: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+}, {
+  sequelize: database,
+  modelName: 'account'
+});
---
+++ back-end/accounts-service/src/models/accounts-repository.ts
@@ -0,0 +1,41 @@
+import { IAccount } from "../interfaces/account-interfaces";
+import accountModel from "./account-model";
+import auth from "../auth";
+
+async function findAll() {
+  return accountModel.findAll();
+}
+
+async function findByPk(id: number) {
+  return accountModel.findByPk(id);
+}
+
+async function addAccount(account: IAccount) {
+  account.password = auth.hashPassword(account.password);
+  return accountModel.create(account);
+}
+
+async function updateAccount(id: number, account: IAccount) {
+  const originalAccount = await accountModel.findByPk(id);
+
+  if (originalAccount != null) {
+    if (account.name) originalAccount.name = account.name;
+    if (account.password) originalAccount.password = account.password;
+    if (account.domain) originalAccount.domain = account.domain;
+
+    await originalAccount.save();
+
+    originalAccount.password = "";
+
+    return originalAccount;
+  }
+
+  return null;
+}
+
+export default {
+  findAll,
+  findByPk,
+  addAccount,
+  updateAccount
+}
