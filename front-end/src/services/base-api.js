85339dc39c9912b9f03752050eac473fff561bb9
Josias Buday Developer <josiasbudaydeveloper@gmail.com>
2023-09-11 00:21:46 -0300

    Improving comments
    At front-end\src\services\base-api.js

--- front-end/src/services/base-api.js
+++ front-end/src/services/base-api.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import { getToken } from './auth';
 
+//
+/**
+ Preconfiguring the requests:
+ - Sets the baseURL
+ - Sets the acess token to the authorization header of the requests
+ * @param { String } baseURL - The base URL of the respective API
+ * @returns 
+ */
 export default function baseAPI(baseURL) {
   const api = axios.create({
     baseURL,
